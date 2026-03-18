1f88ee5f9a031c5bd293ffd54751d2f5cead46a4
Lennon Garcia D. Bravo <5113785+greenlink@users.noreply.github.com>
2026-03-17 22:01:57 -0300

    modify: home page under construction.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 function Home() {
-  return <h1>Dri, eu amo você. Se você me ama, dá uma risadinha! 🥰</h1>;
+  return (
+    <>
+      <html lang="pt-BR">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Em Construção</title>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Em construção...</h1>
+            <h3>
+              Seu organizador de tarefas vai chegar voando, feito para ser
+              responsivo em todos os dispositivos.
+            </h3>
+          </div>
+        </body>
+      </html>
+    </>
+  );
 }
 
 export default Home;
