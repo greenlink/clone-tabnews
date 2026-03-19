00939ce42d1c29eadc8f39764cf9f64afffa304b
Lennon Garcia D. Bravo <5113785+greenlink@users.noreply.github.com>
2026-03-18 21:59:50 -0300

    fix: landing page html.

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-function Home() {
+function home() {
   return (
     <>
-      <html lang="pt-BR">
-        <head>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>Em Construção</title>
-        </head>
-        <body>
-          <div class="container">
-            <h1>Em construção...</h1>
-            <h3>
-              Seu organizador de tarefas vai chegar voando, feito para ser
-              responsivo em todos os dispositivos.
-            </h3>
-          </div>
-        </body>
-      </html>
+      <h1>Em construção...</h1>
+      <h3>
+        Seu organizador de tarefas vai chegar voando, feito para ser responsivo
+        em todos os dispositivos.
+      </h3>
     </>
   );
 }
 
-export default Home;
+export default home;
