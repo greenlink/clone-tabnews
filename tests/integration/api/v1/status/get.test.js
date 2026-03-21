7c36412c60fbd8b16ef870fab4e38e74737bab23
Lennon Garcia D. Bravo <5113785+greenlink@users.noreply.github.com>
2026-03-21 15:24:23 -0300

    modify: finish `/api/v1/status` endpoint.

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVersion = "16.0";
+  const maxConnections = 100;
+  const openedConnections = 1;
+  expect(responseBody.dependencies.database.version).toEqual(postgresVersion);
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    maxConnections,
+  );
+  expect(responseBody.dependencies.database.opened_connections).toEqual(
+    openedConnections,
+  );
 });
